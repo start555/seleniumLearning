b16301b534403ed4cb76788c9118510bfbc143a4
homkas <marta.verenchikova@gmail.com>
2016-09-30 19:51:49 +0300

    Update PageBase.java
    Update method with "flag" (change state from true to false)

--- ExampleWithAllPageElement/src/main/java/core/PageBase.java
+++ ExampleWithAllPageElement/src/main/java/core/PageBase.java
@@ -5,11 +5,13 @@ import org.openqa.selenium.WebDriver;
 
 public class PageBase {
 
-    protected static boolean checkExpectedElementsWithVarargs(WebDriver driver, By...elements){
-        for (int i = 0; i < elements.length; i++){
-            if (driver.findElement(elements[i]).isDisplayed() == true){
-                return true;
+    protected static boolean checkExpectedElementsWithVarargs(WebDriver driver, By...elements) {
+        boolean allElementsAreVisible = true;
+        for (By element : elements) {
+            if (!driver.findElement(element).isDisplayed()) {
+                return false;
             }
-        } return false;
+        }
+        return allElementsAreVisible;
     }
 }
